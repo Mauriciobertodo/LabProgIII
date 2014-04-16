9a183a15b47b632731edcd863764f522db8a50b7
Mauriciobertodo <mauriciobertodo@gmail.com>
2014-04-16 13:20:15 -0300

    CrudPessoa versao 1
    Primeiro commit

---
+++ CrudPessoa/src/crudpessoa/CrudPessoa.java
@@ -0,0 +1,10 @@
+
+package crudpessoa;
+
+public class CrudPessoa {
+
+    public static void main(String[] args) {
+        JanelaPrincipal iniciaJanela = new JanelaPrincipal();
+        iniciaJanela.Teste();
+    }    
+}
---
+++ CrudPessoa/src/crudpessoa/JanelaPrincipal.java
@@ -0,0 +1,9 @@
+
+package crudpessoa;
+
+public class JanelaPrincipal {
+    
+    public void Teste(){
+        System.out.println("MOSTRA NA TELA");
+    }    
+}
