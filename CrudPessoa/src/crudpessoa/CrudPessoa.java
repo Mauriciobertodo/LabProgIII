a7a3e639b3b02e97aaea0900827fa738d3aa0377
Mauriciobertodo <mauriciobertodo@gmail.com>
2014-04-20 13:42:17 -0300

    CrudPessoa versao 2
    Tela Principal criada

--- CrudPessoa/src/crudpessoa/CrudPessoa.java
+++ CrudPessoa/src/crudpessoa/CrudPessoa.java
@@ -4,7 +4,6 @@ package crudpessoa;
 public class CrudPessoa {
 
     public static void main(String[] args) {
-        JanelaPrincipal iniciaJanela = new JanelaPrincipal();
-        iniciaJanela.Teste();
-    }    
+        new JanelaPrincipal();
+    }
 }
--- CrudPessoa/src/crudpessoa/JanelaPrincipal.java
+++ CrudPessoa/src/crudpessoa/JanelaPrincipal.java
@@ -1,9 +1,44 @@
-
 package crudpessoa;
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+import javax.swing.DefaultListModel;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JList;
+import javax.swing.JPanel;
 
-public class JanelaPrincipal {
+public class JanelaPrincipal extends JFrame {
     
-    public void Teste(){
-        System.out.println("MOSTRA NA TELA");
-    }    
+    private JFrame janela = new JFrame();
+    JList<String> listaPessoas;
+    private JPanel painel = new JPanel();
+    private JButton botaoNovo = new JButton("Novo");
+    private JButton botaoEditar = new JButton("Editar");
+    private JButton botaoRemover = new JButton("Remover");
+    
+    public JanelaPrincipal(){
+        
+        setTitle("Pessoas");
+        setSize(250,400);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        montaJanela(); // inclui componentes na janela principal
+        setLocationRelativeTo(null);
+        setVisible(true);
+    }
+    public void montaJanela(){
+        
+        listaPessoas = new JList<>();
+        
+        painel.setLayout(new FlowLayout());
+        painel.add(botaoNovo);
+        painel.add(botaoEditar);
+        painel.add(botaoRemover);
+        
+        getContentPane().setLayout(new BorderLayout());
+        getContentPane().add(listaPessoas,BorderLayout.CENTER);
+        getContentPane().add(painel,BorderLayout.SOUTH);
+        
+    }
+    
+    
 }
